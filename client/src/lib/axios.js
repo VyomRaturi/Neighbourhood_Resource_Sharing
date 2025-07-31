4d214df34d7d9fc9112c74e2fa18135b7a7bb963
Vyom <vyomraturi2004@gmail.com>
2025-07-31 18:08:35 +0530

    updated backend url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://neighbourhood-resource-sharing.vercel.app/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
