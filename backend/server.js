9a736a842595fc7a4faae78b56e23af108329e79
Vyom <vyomraturi2004@gmail.com>
2025-07-31 18:23:15 +0530

    updated frontend url

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration for production
 const corsOptions = {
-  origin: "https://neighbourhood-resource-sharing.vercel.app/",
+  origin: "https://neighbourhood-resource-sharing.vercel.app",
   credentials: true,
   optionsSuccessStatus: 200
 };
